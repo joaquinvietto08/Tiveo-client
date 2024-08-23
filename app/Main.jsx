cb425192fa56e87a5988d68d1104c1441c348b80
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-08-23 19:40:45 -0300

    revert to cancell wrong merge

--- app/Main.jsx
+++ app/Main.jsx
@@ -9,10 +9,6 @@ import Profile from "./profile/Profile";
 import TabBar from "../components/TabBar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ActivityDetail from "./activity/ActivityDetail";
-import Welcome from "./Welcome";
-import Login from "./auth/Login";
-import auth from "@react-native-firebase/auth";
-import { useEffect, useState } from "react";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -57,36 +53,13 @@ function MainNavigator() {
 }
 
 const Main = () => {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
-
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // Unsubscribe on unmount
-  }, []);
-
-  if (initializing) return null;
   return (
     <SafeAreaProvider>
       <NavigationContainer style={{}}>
         <StatusBar style="dark" />
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {user ? (
-            <>
-              <Stack.Screen name="MainNavigator" component={MainNavigator} />
-              <Stack.Screen name="ActivityDetail" component={ActivityDetail} />
-            </>
-          ) : (
-            <>
-              <Stack.Screen name="Welcome" component={Welcome} />
-              <Stack.Screen name="Login" component={Login} />
-            </>
-          )}
+          <Stack.Screen name="MainNavigator" component={MainNavigator} />
+          <Stack.Screen name="ActivityDetail" component={ActivityDetail} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
@@ -94,3 +67,16 @@ const Main = () => {
 };
 
 export default Main;
+
+/*
+const Main = () => {
+  return (
+    <View style={{ flexGrow: 1 }}>
+      <StatusBar style="dark" />
+      <Home />
+    </View>
+  );
+};
+
+export default Main;
+*/
--- app/profile/Profile.jsx
+++ app/profile/Profile.jsx
@@ -1,56 +1,12 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
-import auth from "@react-native-firebase/auth";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-const Profile = ({ navigation }) => {
-  const handleSignOut = async () => {
-    try {
-      // Revocar acceso de Google
-      await GoogleSignin.revokeAccess();
-      console.log("Google access revoked!");
-
-      // Cerrar sesión de Firebase
-      await auth().signOut();
-      console.log("User signed out successfully!");
-    } catch (error) {
-      console.error("Error signing out: ", error);
-      alert("Sign out failed: " + error.message);
-    }
-  };
-
+const Profile = () => {
   return (
-    <View style={styles.container}>
-      <Text>Agregar celular</Text>
-      <Pressable
-        style={styles.button}
-        //      onPress={() => navigation.navigate("AddPhone")}
-      >
-        <Text>Details</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={handleSignOut}>
-        <Text>Cerrar sesion</Text>
-      </Pressable>
+    <View>
+      <Text>Profile</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    marginBottom: 20,
-    width: 200,
-    height: 40,
-    backgroundColor: "#FDD146",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-  },
-});
 
 export default Profile;
