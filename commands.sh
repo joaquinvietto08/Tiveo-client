2746c8e3de3661f911d96723d86a5fb7a4721690
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-08-23 18:03:38 -0300

    Firebase packages

--- commands.sh
+++ commands.sh
@@ -2,7 +2,37 @@
 npx expo install react-native-reanimated react-native-gesture-handler
 npm install @gorhom/bottom-sheet
 
-*Add in babel
-plugins: ["react-native-reanimated/plugin"],
+#Add on babel
+plugins: ["react-native-reanimated/plugin"]
+
+#################################################
+
+#Firebase
+npx expo install expo-router expo-linking
+npx expo install expo-build-properties
+npx expo install @react-native-firebase/app
+npx expo install @react-native-firebase/auth
+npm install @react-native-async-storage/async-storage //for the moment firebase handles this, it is not necessary
+npm install firebase
+
+#Signin with Google
+npx expo install @react-native-google-signin/google-signin
+
+#Create firebaseConfig.js
+
+#Make changes on app.json
+
+#Create andorid/ios fold (If it returns an error but the folders are created, it's okay)
+npx expo prebuild
+
+#Get SHA1 Certificate Fingerprints for firebase
+cd .\android\
+./gradlew signinReport 
+
+#Add android/ios .json firebase's files and re-run 
+npx expo prebuild
+
+#Doc
+https://rnfirebase.io/
 
 #################################################
