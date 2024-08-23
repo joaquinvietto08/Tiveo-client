0b78922a25065829c87e4a47ec53b4a3a18549e1
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-08-23 18:58:35 -0300

    Main changes

--- app/Main.jsx
+++ app/Main.jsx
@@ -11,6 +11,8 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import ActivityDetail from "./activity/ActivityDetail";
 import Welcome from "./Welcome";
 import Login from "./auth/Login";
+import auth from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -55,15 +57,36 @@ function MainNavigator() {
 }
 
 const Main = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // Unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
   return (
     <SafeAreaProvider>
       <NavigationContainer style={{}}>
         <StatusBar style="dark" />
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="MainNavigator" component={MainNavigator} />
-          <Stack.Screen name="ActivityDetail" component={ActivityDetail} />
+          {user ? (
+            <>
+              <Stack.Screen name="MainNavigator" component={MainNavigator} />
+              <Stack.Screen name="ActivityDetail" component={ActivityDetail} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Welcome" component={Welcome} />
+              <Stack.Screen name="Login" component={Login} />
+            </>
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
@@ -71,16 +94,3 @@ const Main = () => {
 };
 
 export default Main;
-
-/*
-const Main = () => {
-  return (
-    <View style={{ flexGrow: 1 }}>
-      <StatusBar style="dark" />
-      <Home />
-    </View>
-  );
-};
-
-export default Main;
-*/
