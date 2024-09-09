10405e6928da7aa03e74f8854bd9bbba9d96f442
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-09-08 22:45:03 -0300

    changes

--- app/Map.js
+++ app/Map.js
@@ -1,35 +1,28 @@
 import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { mapStyle } from "../components/map/mapStyle";
 import { mapConfig } from "../components/map/mapConfig";
 
 const Map = () => {
   return (
-    <View style={styles.container}>
-      <MapView
-        customMapStyle={mapStyle}
-        provider={PROVIDER_GOOGLE}
-        style={styles.mapStyle}
-        initialRegion={{
-          latitude: -34.603684,
-          longitude: -58.381559,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        mapType="standard"
-        {...mapConfig}
-      ></MapView>
-    </View>
+    <MapView
+      customMapStyle={mapStyle}
+      provider={PROVIDER_GOOGLE}
+      style={styles.mapStyle}
+      initialRegion={{
+        latitude: -34.603684,
+        longitude: -58.381559,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+      mapType="standard"
+      {...mapConfig}
+    ></MapView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   mapStyle: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
--- app/home/Home.jsx
+++ app/home/Home.jsx
@@ -8,6 +8,7 @@ import {
   ImageBackground,
   ScrollView,
   FlatList,
+  StatusBar,
 } from "react-native";
 import Tiveo from "../../assets/svgs/tiveo";
 import Electricity from "../../assets/svgs/home/electricity";
@@ -32,6 +33,11 @@ import Photography from "../../assets/svgs/home/photography";
 import Music from "../../assets/svgs/home/music";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Map from "../Map";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import image from "../../assets/images/data/2.png";
+import Feather from "@expo/vector-icons/Feather";
 
 const Home = ({ navigation }) => {
   const insets = useSafeAreaInsets();
@@ -39,10 +45,46 @@ const Home = ({ navigation }) => {
     <View
       style={{
         ...styles.mainContainer,
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
       }}
-    ></View>
+    >
+      <StatusBar translucent barStyle="dark-content" />
+      <Map />
+      <View
+        style={{
+          ...styles.container,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        }}
+      >
+        <View style={styles.headerContainer}>
+          <View style={styles.user_picContainer}>
+            <Pressable style={styles.user_picButton}>
+              <Image source={image} style={styles.picProfile} />
+            </Pressable>
+          </View>
+          <View style={styles.locationContainer}>
+            <Pressable style={styles.locationButton}>
+              <MaterialIcons name="location-pin" size={24} color="black" />
+              <Text
+                style={styles.locationText}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                Marcelo T de Alvear 360
+              </Text>
+              <Icon name="keyboard-arrow-down" size={29} color="#000" />
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.searchContainer}>
+          <Pressable style={styles.searchButton}>
+            <Ionicons name="search-outline" size={24} color="black" />
+            <Text style={{ marginLeft: 10, fontSize: 16 }}>Buscar...</Text>
+            <Feather name="grid" size={24} color="black" />
+          </Pressable>
+        </View>
+      </View>
+    </View>
   );
 };
 
@@ -50,6 +92,72 @@ const styles = StyleSheet.create({
   mainContainer: {
     backgroundColor: "#F5F5F5",
     flex: 1,
+  },
+  container: {
+    position: "absolute",
+    width: "100%",
+  },
+  headerContainer: {
+    width: "100%",
+    height: 90,
+    paddingTop: 30,
+    justifyContent: "flex-end",
+  },
+  user_picContainer: {
+    justifyContent: "flex-end",
+    width: "100%",
+    alignItems: "flex-end",
+    position: "absolute",
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  user_picButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  picProfile: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  locationContainer: {
+    alignItems: "center",
+    width: "100%",
+    position: "absolute",
+  },
+  locationButton: {
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: 215,
+  },
+  locationText: {
+    fontWeight: "bold",
+    fontSize: 14,
+    overflow: "hidden",
+    maxWidth: 160,
+  },
+  searchContainer: {
+    width: "100%",
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    paddingTop: 15,
+  },
+  searchButton: {
+    width: "100%",
+    height: 53,
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 22,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 1,
   },
 });
 
